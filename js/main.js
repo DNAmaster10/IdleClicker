1dd81204196199b9275f7370785c7dc915890281
DNAmaster10 <louishollobon@outlook.com>
2022-12-15 21:21:33 +0000

    First Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var totalGuests = 0;
+
+var i = 1;
+
+function myLoop() {
+  setTimeout(function() {
+    console.log('hello');
+    i++;
+    if (i < 10) {
+      myLoop();
+    }
+  }, 500
+}
+
+myLoop();
